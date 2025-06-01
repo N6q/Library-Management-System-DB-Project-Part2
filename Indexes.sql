c57cda725cf93f5a70674fbdb49f8f6d0f8a0e2c
Samir Al-Bulushi <albulushii.samir@gmail.com>
2025-06-01 14:26:33 +0400

    Add Non-clustered index on Location

--- Indexes.sql
+++ Indexes.sql
@@ -1,4 +1,7 @@
--- Non-clustered index on Name 
+-- Non-clustered index on Name (for searching by name)
 CREATE NONCLUSTERED INDEX IX_Library_Name
 ON Libraries(Name);
 
+-- Non-clustered index on Location (for filtering by location)
+CREATE NONCLUSTERED INDEX IX_Library_Location
+ON Libraries(Location);
