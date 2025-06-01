fcca10e283ad4c1883c6fcb483c6a33e8620763b
Samir Al-Bulushi <albulushii.samir@gmail.com>
2025-06-01 14:49:14 +0400

    Add ViewPopularBooks: Books with average rating > 4.5 + total loans

---
+++ Views.sql
@@ -0,0 +1,21 @@
+﻿
+
+-- ViewPopularBooks: Books with average rating > 4.5 + total loans
+CREATE VIEW ViewPopularBooks AS
+SELECT 
+    b.BookID,
+    b.Title,
+    AVG(r.Rating) AS AverageRating,
+    COUNT(l.LoanID) AS TotalLoans
+FROM 
+    Books b
+    LEFT JOIN Reviews r 
+		ON b.BookID = r.BookID
+    LEFT JOIN Loans l 
+		ON b.BookID = l.BookID
+GROUP BY 
+    b.BookID, b.Title
+HAVING 
+    AVG(r.Rating) > 4.5;
+	
+
